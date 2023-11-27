dc3118ca93d10de8d4436ee3e591069b58112588
Francesco Rocco <francescorocco97@gmail.com>
2023-11-27 17:06:24 +0100

    Aggiunta migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 50)->nullable();
+            $table->string('surname', 50)->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->date('birth_date')->nullable();
             $table->timestamps();
         });
     }
