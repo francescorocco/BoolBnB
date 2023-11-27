149436aa3c7b4ae2f82c67976593e080fd1645aa
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-11-27 17:06:18 +0100

    add apartements seeder

---
+++ database/seeders/ApartmentsTableSeeder.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ApartmentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Camere all'interno del sito
+        $apartements = [
+            [
+                "user_id"       => 1,
+                "name"          => "Milano",
+                "rooms"         => "4",
+                "beds"          => "",
+                "bathrooms"     => "",
+                "mq"            => "100",
+                "address"       => "",
+                "lat"           => "",
+                "lon"           => "",
+                "photo"         => "",
+                "visible"       => "si",
+
+            ],
+
+            [
+                "user_id"       => 2,
+                "name"          => "Roma",
+                "rooms"         => "8",
+                "beds"          => "",
+                "bathrooms"     => "",
+                "mq"            => "",
+                "address"       => "",
+                "lat"           => "",
+                "lon"           => "",
+                "photo"         => "",
+                "visible"       => "",
+
+            ],
+
+            [
+                "user_id"       => 3,
+                "name"          => "",
+                "rooms"         => "",
+                "beds"          => "",
+                "bathrooms"     => "",
+                "mq"            => "",
+                "address"       => "",
+                "lat"           => "",
+                "lon"           => "",
+                "photo"         => "",
+                "visible"       => "",
+
+            ],
+
+            [
+                "user_id"       => 4,
+                "name"          => "",
+                "rooms"         => "",
+                "beds"          => "",
+                "bathrooms"     => "",
+                "mq"            => "",
+                "address"       => "",
+                "lat"           => "",
+                "lon"           => "",
+                "photo"         => "",
+                "visible"       => "",
+
+            ],
+
+            [
+                "user_id"       => 5,
+                "name"          => "",
+                "rooms"         => "",
+                "beds"          => "",
+                "bathrooms"     => "",
+                "mq"            => "",
+                "address"       => "",
+                "lat"           => "",
+                "lon"           => "",
+                "photo"         => "",
+                "visible"       => "",
+
+            ],
+
+            [
+                "user_id"       => 6,
+                "name"          => "",
+                "rooms"         => "",
+                "beds"          => "",
+                "bathrooms"     => "",
+                "mq"            => "",
+                "address"       => "",
+                "lat"           => "",
+                "lon"           => "",
+                "photo"         => "",
+                "visible"       => "",
+
+            ],
+
+            [
+                "user_id"       => 7,
+                "name"          => "",
+                "rooms"         => "",
+                "beds"          => "",
+                "bathrooms"     => "",
+                "mq"            => "",
+                "address"       => "",
+                "lat"           => "",
+                "lon"           => "",
+                "photo"         => "",
+                "visible"       => "",
+
+            ],
+
+            [
+                "user_id"       => 8,
+                "name"          => "",
+                "rooms"         => "",
+                "beds"          => "",
+                "bathrooms"     => "",
+                "mq"            => "",
+                "address"       => "",
+                "lat"           => "",
+                "lon"           => "",
+                "photo"         => "",
+                "visible"       => "",
+
+            ],
+
+            [
+                "user_id"       => 9,
+                "name"          => "",
+                "rooms"         => "",
+                "beds"          => "",
+                "bathrooms"     => "",
+                "mq"            => "",
+                "address"       => "",
+                "lat"           => "",
+                "lon"           => "",
+                "photo"         => "",
+                "visible"       => "",
+
+            ],
+
+            [
+                "user_id"       => 10,
+                "name"          => "",
+                "rooms"         => "",
+                "beds"          => "",
+                "bathrooms"     => "",
+                "mq"            => "",
+                "address"       => "",
+                "lat"           => "",
+                "lon"           => "",
+                "photo"         => "",
+                "visible"       => "",
+
+            ],
+        ]
+    }
+}
