2aadffab4238ece701d82c366df41ecaa3de8f65
Francesco Rocco <francescorocco97@gmail.com>
2023-11-28 11:01:40 +0100

    Create migrazioni

---
+++ database/migrations/2023_11_28_095323_create_apartment_sponsorship_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_sponsorship', function (Blueprint $table) {
+            $table->unsignedBigInteger('apartemnt_id');
+            $table->foreign('apartemnt_id')->references('id')->on('apartments')->onDelete('CASCADE');
+
+            $table->unsignedBigInteger('sponsorship_id');
+            $table->foreign('sponsorship_id')->references('id')->on('sponsorships')->onDelete('CASCADE');
+
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('expire_date')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_sponsorship');
+    }
+};
