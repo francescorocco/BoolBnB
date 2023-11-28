8965d46237a8742f52cdf79e5286c7384a62a172
Francesco Rocco <francescorocco97@gmail.com>
2023-11-28 11:10:16 +0100

    Creati le migration per servizi

---
+++ database/migrations/2023_11_28_100822_create_apartment_service_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_service', function (Blueprint $table) {
+            $table->unsignedBigInteger('apartemnt_id');
+            $table->foreign('apartemnt_id')->references('id')->on('apartments')->onDelete('CASCADE');
+
+            $table->unsignedBigInteger('service_id');
+            $table->foreign('service_id')->references('id')->on('services')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_service');
+    }
+};
