9307ba4fa2073bb8bc3119f19e59f119067a7cc7
Francesco Rocco <francescorocco97@gmail.com>
2023-11-29 16:29:46 +0100

    Modificato metodo di seed user tramite factori

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,21 +3,27 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = User::class;
+
     public function definition()
     {
+
+        $firstName = $this->faker->unique()->firstName();
+        $lastName = $this->faker->lastName();
+
         return [
-            //
+            'name'  =>  $firstName,
+            'lastname'  =>  $lastName,
+            'email' =>  strtolower($firstName . '.' . $lastName . 'test.com'),
+            'password'  =>  bcrypt('asdfasdf'),
         ];
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,56 +15,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // Array di utenti registrati al sito Bnb
-        $users = [
-            [
-                "name"      => "Gianfranco",
-                "lastname"  => "Orlandini",
-                "email"     => "giafranco.orlandini@gmail.com",
-                "password"  => bcrypt("gdfhsgbdjynr"),
-
-            ],
-
-            [
-                "name"      => "Vincenzo",
-                "lastname"  => "Di vita",
-                "email"     => "vincenzo.divita@gmail.com",
-                "password"  => bcrypt("hgnbfhhg"),
-            ],
-
-            [
-                "name"      => "Gabriella",
-                "lastname"  => "Marchese",
-                "email"     => "gabriella.marchese@gmail.com",
-                "password"  => bcrypt("4reyfhgy"),
-            ],
-
-            [
-                "name"      => "Irene",
-                "lastname"  => "Caiazzo",
-                "email"     => "irene.caiazzo@gmail.com",
-                "password"  => bcrypt("thrgefbhtrg"),
-            ],
-
-            [
-                "name"      => "Vincenzo",
-                "lastname"  => "Minardi",
-                "email"     => "vincenzo.minardi@gmail.com",
-                "password"  => bcrypt("rhfgegrge65r"),
-
-            ],
-
-            [
-                "name"      => "Antonio",
-                "lastname"  => "Nicolaci",
-                "email"     => "antonio.nicolaci@gmail.com",
-                "password"  => bcrypt("sa187el"),
-            ],
-        ];
-
-        foreach ($users as $user) {
-            //creare riga nel DB
-            User::create($user);
-        }
+        User::factory()->count(6)->create();
     }
 }
